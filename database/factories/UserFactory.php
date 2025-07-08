998fac11eb1851d0d87d9fee574020eeac3d9938
Ali-Awad <aliauad2004@gmail.com>
2025-07-08 22:08:44 +0300

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
-            'role' => 'member', // Default role
+            'role' => 'member', 
         ];
     }
 
