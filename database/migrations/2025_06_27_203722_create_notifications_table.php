af76d473b0ad01df72696782620d70d2c3bcf465
Ali-Awad <aliauad2004@gmail.com>
2025-07-08 22:12:09 +0300

    Update 2025_06_27_203722_create_notifications_table.php

--- database/migrations/2025_06_27_203722_create_notifications_table.php
+++ database/migrations/2025_06_27_203722_create_notifications_table.php
@@ -11,10 +11,10 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('notifications', function (Blueprint $table) {
-            $table->uuid('id')->primary(); // Using UUID for notifications
+            $table->uuid('id')->primary(); 
             $table->string('type');
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // Recipient of the notification
-            $table->text('data'); // JSON data for notification details
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); 
+            $table->text('data'); 
             $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });
